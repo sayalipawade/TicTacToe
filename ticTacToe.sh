649c6d0396d9b7d025d36d831ae6e093bbb068a7
sayalipawade <sayalipawade05@gmail.com>
2020-03-27 15:36:41 +0530

    Added:Resettigboard before playing game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,24 @@
 #!/bin/bash -x
 echo "Welcome"
+
+#variables
+no=1
+
+#printing the fresh board
+declare -A board
+function freshBoard()
+{
+	for(( i=0;i<3;i++ ))
+	do
+		for (( j=0;j<3;j++ ))
+		do
+         board[$i,$j]=$(( no++ ))
+			printf "[""${board[$i,$j]}""]"
+      done
+      printf "\n"
+   done
+}
+freshBoard
+
+
+
