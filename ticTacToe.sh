60bd970adcfdf3d828565e18678af681f411ad56
sayalipawade <sayalipawade05@gmail.com>
2020-03-27 15:57:41 +0530

    Added:Assigning letter X or O to the player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,24 +1,37 @@
 #!/bin/bash -x
 echo "Welcome"
 
+#constants
+PLAYER1=""
+PLAYER2=""
+
 #variables
 no=1
 
 #printing the fresh board
 declare -A board
 function freshBoard()
-{
-	for(( i=0;i<3;i++ ))
+{ 
+	for (( i=0;i<3;i++ ))
 	do
-		for (( j=0;j<3;j++ ))
+		for(( j=0;j<3;j++ ))
 		do
-         board[$i,$j]=$(( no++ ))
+			board[$i,$j]=$(( no++ ))
 			printf "[""${board[$i,$j]}""]"
-      done
-      printf "\n"
-   done
+		done
+		printf "\n"
+	done
 }
-freshBoard
+
+#letter assignment
+randomNo=$((RANDOM%2))
+if [[ $randomNo -eq 1 ]]
+then
+	PLAYER1="X"
+else
+	PLAYER2="O"
+fi
+
 
 
 
